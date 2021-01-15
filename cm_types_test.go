b737c53a1fd2f68ebed81118b93a724d558723ad
Dave Henderson <dhenderson@gmail.com>
2021-01-15 15:39:06 -0500

    Ignore malformed PortInfo entries
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- cm_types_test.go
+++ cm_types_test.go
@@ -1,6 +1,7 @@
 package hitron
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
@@ -27,4 +28,100 @@ func TestParseDHCPLeaseTime(t *testing.T) {
 			assert.Equal(t, d.dur, out)
 		})
 	}
+}
+
+func TestPortInfoUnmarshalJSON(t *testing.T) {
+	in := `{
+	"portId": "1",
+	"frequency": "615000000",
+	"modulation": "QAM256",
+	"signalStrength": "2.500",
+	"snr": "37.636",
+	"channelId": "11",
+	"dsoctets": "20883398",
+	"correcteds": "1",
+	"uncorrect": "0"
+}`
+	expected := &PortInfo{
+		PortID:         "1",
+		Frequency:      615000000,
+		Modulation:     "QAM256",
+		SignalStrength: 2.5,
+		ChannelID:      "11",
+		SNR:            37.636,
+		DsOctets:       20883398,
+		Correcteds:     1,
+		Uncorrect:      0,
+	}
+	out := &PortInfo{}
+	err := json.Unmarshal([]byte(in), out)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, out)
+
+	in = `{
+	"portId": "8",
+	"frequency": "",
+	"modulation": "QAM256",
+	"signalStrength": false,
+	"snr": false,
+	"channelId": "0",
+	"dsoctets": "0",
+	"correcteds": "0",
+	"uncorrect": "0"
+}`
+	out = &PortInfo{}
+	err = json.Unmarshal([]byte(in), out)
+	assert.Error(t, err)
+}
+
+func TestCMDsInfoUnmarshalJSON(t *testing.T) {
+	in := `{
+	"errCode": "000",
+	"errMsg": "",
+	"Freq_List": [
+		{
+		"portId": "1",
+		"frequency": "615000000",
+		"modulation": "QAM256",
+		"signalStrength": "2.500",
+		"snr": "37.636",
+		"channelId": "11",
+		"dsoctets": "20883398",
+		"correcteds": "1",
+		"uncorrect": "0"
+		},
+		{
+		"portId": "8",
+		"frequency": "",
+		"modulation": "QAM256",
+		"signalStrength": false,
+		"snr": false,
+		"channelId": "0",
+		"dsoctets": "0",
+		"correcteds": "0",
+		"uncorrect": "0"
+		}
+	]
+}`
+	expected := &CMDsInfo{
+		Error: NoError,
+		Ports: []PortInfo{
+			{
+				PortID:         "1",
+				Frequency:      615000000,
+				Modulation:     "QAM256",
+				SignalStrength: 2.5,
+				ChannelID:      "11",
+				SNR:            37.636,
+				DsOctets:       20883398,
+				Correcteds:     1,
+				Uncorrect:      0,
+			},
+		},
+	}
+
+	out := &CMDsInfo{}
+	err := json.Unmarshal([]byte(in), out)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, out)
 }
